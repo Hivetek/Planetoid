217ca8c357e7db26cf8b021f912ddf3fda781818
Peter Severin Rasmussen <git@petersr.com>
2015-08-05 15:33:01 +0200

    Added object manipulation functions to core.

--- game/shared/core.js
+++ game/shared/core.js
@@ -16,8 +16,8 @@ Core.clone = function(obj) {
 };
 
 Core.override = function(obj, override) {
-    if (typeof obj !== "object") return;
-    if (typeof override !== "object") return;
+    if (typeof obj !== "object") throw new TypeError("Core.override: obj is not an object");
+    if (typeof override !== "object") throw new TypeError("Core.override: override is not an object");
 
     for (var key in override) {
         var newValue = override[key];
@@ -30,6 +30,78 @@ Core.override = function(obj, override) {
         }
     }
 
+    return obj;
+};
+
+Core.deletions = function(obj, deletions) {
+    if (typeof obj !== "object") throw new TypeError("Core.deletions: obj is not an object");
+
+    // Check if we are dealing with an array
+    if (Object.prototype.toString.call(deletions) === '[object Array]') {
+        var i, j,
+            len = deletions.length,
+            deletion, split,
+            splitLen, splitElem,
+            descension;
+        for (i = 0; i < len; i++) {
+            deletion = deletions[i];
+            split = deletion.split(".");
+            splitLen = split.length
+            descension = obj;
+            for (j = 0; j < splitLen; j++) {
+                splitElem = split[j];
+                if (descension.hasOwnProperty(splitElem)) {
+                    if (j < splitLen-1) {
+                        descension = descension[splitElem];
+                    } else {
+                        delete descension[splitElem];
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+    } else if (typeof deletions === "object") {
+
+    } else {
+        throw new TypeError("Core.deletions: deletions is neither object nor array");
+    }
+
+    return obj;
+};
+
+Core.insertions = function(obj, insertions) {
+    if (typeof obj !== "object") throw new TypeError("Core.insertions: obj is not an object");
+    if (typeof insertions !== "object") throw new TypeError("Core.insertions: insertions is not an object");
+
+    for (var key in insertions) {
+        var insertion = insertions[key];
+        if (obj.hasOwnProperty(key)) {
+            var existing = obj[key];
+            if (typeof existing === "object") {
+                Core.insertions(existing, insertion);
+            } // Else, do not override
+        } else {
+            obj[key] = insertion;
+        }
+    }
+
+    return obj;
+};
+
+Core.sync = function(obj, instructions) {
+    if (instructions.hasOwnProperty("delete")) {
+        var deletions = instructions["delete"];
+        Core.deletions(obj, deletions);
+    }
+    if (instructions.hasOwnProperty("insert")) {
+        var insertions = instructions["insert"];
+        Core.insertions(obj, insertions);
+    }
+    if (instructions.hasOwnProperty("update")) {
+        var updates = instructions["update"];
+        Core.override(obj, updates);
+    }
     return obj;
 };
 
